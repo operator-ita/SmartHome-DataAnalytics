f78dafa694546ceba64c9c2ca46bb6c32d96a78d
Luis Fer Jiménez <>
2021-02-05 20:42:14 -0600

    Created Exploracion.R

---
+++ EDA/Exploracion.R
@@ -0,0 +1,7 @@
+# @Proyecto: 
+# @Equipo: 14
+
+# En este script se importan los datasets de las dos casas del proyecto "Aras"
+# previamente limpiados y se hace un análisis exploratorio para el planteamiento
+# de los problemas del proyecto.
+
