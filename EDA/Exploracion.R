24066b51da5c4ee310d16ffe791b428ae6b207a6
Luis Fer Jiménez <>
2021-02-08 18:52:01 -0600

    update Exploracion.R

--- EDA/Exploracion.R
+++ EDA/Exploracion.R
@@ -13,6 +13,9 @@
 library(dplyr)
 library(plyr)
 library(tibble)
+library(ggplot2)
+library(scales)
+library(ggthemes)
 
 # Directorio de trabajo
 repo.dir <- "C:/Users/luisf/Github/SmartHome-DataAnalytics"
@@ -49,11 +52,70 @@ write.csv(houseB.P2.Freq, file='data/Frequency/houseB_P2.csv', row.names=FALSE)
 write.csv(houseA.Freq, file='data/Frequency/houseA.csv', row.names=TRUE)
 write.csv(houseB.Freq, file='data/Frequency/houseB.csv', row.names=TRUE)
 
+# Bar Charts
+# Convirtir tablas a dataframes
+(freq_table_HA_R1 <- as.data.frame(houseA.P1.Freq))
+names(freq_table_HA_R1) <- c("Activity", "Freq")
+(freq_table_HA_R2 <- as.data.frame(houseA.P2.Freq))
+names(freq_table_HA_R2) <- c("Activity", "Freq")
+(freq_table_HB_R1 <- as.data.frame(houseB.P1.Freq))
+names(freq_table_HB_R1) <- c("Activity", "Freq")
+(freq_table_HB_R2 <- as.data.frame(houseB.P2.Freq))
+names(freq_table_HB_R2) <- c("Activity", "Freq")
 
+# Ordenando por frecuencias
+freq_table_HA_R1[order(freq_table_HA_R1$Freq, decreasing = TRUE),]    
+freq_table_HA_R2[order(freq_table_HA_R2$Freq, decreasing = TRUE),]
+freq_table_HB_R1[order(freq_table_HB_R1$Freq, decreasing = TRUE),]
+freq_table_HB_R2[order(freq_table_HB_R2$Freq, decreasing = TRUE),]
 
+# Creation of bar charts
+bar_HA_R1 <- ggplot(data = freq_table_HA_R1, aes(x = reorder(Activity, Freq), y = Freq)) +
+  geom_col(aes(fill = Activity) , show.legend = FALSE) +
+  ggtitle(paste("Time spent on each activity by resident 1 in house A during 1 month")) +
+  coord_flip() +
+  geom_label(aes(label = percent(Freq/sum(Freq), accuracy =  0.001),
+                 y = -150, fill = Activity),
+             show.legend = FALSE,
+             size = 3.5, label.padding = unit(0.1, "lines")) +
+  expand_limits(y = -150)+
+  labs(x = "Activity", y = "Seconds")
+ggsave("images/Frequency/houseA_P1.png")
 
+(bar_HA_R2 <- ggplot(data = freq_table_HA_R2, aes(x = reorder(Activity, Freq), y = Freq)) +
+  geom_col(aes(fill = Activity) , show.legend = FALSE) +
+  ggtitle(paste("Time spent on each activity by resident 2 in house A during 1 month")) +
+  coord_flip() +
+  geom_label(aes(label = percent(Freq/sum(Freq), accuracy =  0.001),
+                 y = -150, fill = Activity),
+             show.legend = FALSE,
+             size = 3.5, label.padding = unit(0.1, "lines")) +
+  expand_limits(y = -150)+
+  labs(x = "Activity", y = "Seconds"))
+ggsave("images/Frequency/houseA_P2.png")
 
+(bar_HB_R1 <- ggplot(data = freq_table_HB_R1, aes(x = reorder(Activity, Freq), y = Freq)) +
+  geom_col(aes(fill = Activity) , show.legend = FALSE) +
+  ggtitle(paste("Time spent on each activity by resident 1 in house B during 1 month")) +
+  coord_flip() +
+  geom_label(aes(label = percent(Freq/sum(Freq), accuracy =  0.001),
+                 y = -150, fill = Activity),
+             show.legend = FALSE,
+             size = 3.5, label.padding = unit(0.1, "lines")) +
+  expand_limits(y = -150)+
+  labs(x = "Activity", y = "Seconds"))
+ggsave("images/Frequency/houseB_P1.png")
 
-
+(bar_HB_R2 <- ggplot(data = freq_table_HB_R2, aes(x = reorder(Activity, Freq), y = Freq)) +
+  geom_col(aes(fill = Activity) , show.legend = FALSE) +
+  ggtitle(paste("Time spent on each activity by resident 2 in house B during 1 month")) +
+  coord_flip() +
+  geom_label(aes(label = percent(Freq/sum(Freq), accuracy =  0.001),
+                 y = -150, fill = Activity),
+             show.legend = FALSE,
+             size = 3.5, label.padding = unit(0.1, "lines")) +
+  expand_limits(y = -150)+
+  labs(x = "Activity", y = "Seconds"))
+ggsave("images/Frequency/houseB_P2.png")
 
 setwd(repo.dir)
